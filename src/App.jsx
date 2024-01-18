10822174fb8bbc53c4312b14700d925b3f8e340c
vimarshmishra37 <144466200+vimarshmishra37@users.noreply.github.com>
2024-01-19 01:59:27 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,7 @@ function App() {
   useEffect(() => {
     // Simulate an asynchronous operation (e.g., fetching data) for 3 seconds
     const fetchData = async () => {
-<<<<<<< HEAD
-      await new Promise((resolve) => setTimeout(resolve,5300));
-=======
       await new Promise((resolve) => setTimeout(resolve, 5300));
->>>>>>> parent of 0e895ef (check)
       setLoading(false);
     };
 
