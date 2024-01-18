050c448f6eb7da6c53900fe4caa059b3a2370402
Raghav Goel <goelraghav002@gmail.com>
2024-01-19 00:40:36 +0530

    pta nhi kya hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Banner from './container/Banner/Banner';
-import About from './container/About/About';
-import Footer from './components/Footer/Footer';
-import Team from './container/Team/Team';
-import Gallery from './container/Gallery/Gallery';
-import Sponsors from './container/Sponsors/Sponsors';
-import Preloader from './components/Preloader/Preloader'; // Import the Preloader component
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Banner from "./container/Banner/Banner";
+import About from "./container/About/About";
+import Footer from "./components/Footer/Footer";
+import Team from "./container/Team/Team";
+import Gallery from "./container/Gallery/Gallery";
+import Sponsors from "./container/Sponsors/Sponsors";
+import Preloader from "./components/Preloader/Preloader"; // Import the Preloader component
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     // Simulate an asynchronous operation (e.g., fetching data) for 3 seconds
     const fetchData = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 300));
+      await new Promise((resolve) => setTimeout(resolve, 5300));
       setLoading(false);
     };
 
@@ -33,7 +33,7 @@ function App() {
           <About />
           <Team />
           <Gallery />
-     
+
           <Footer />
         </>
       )}
