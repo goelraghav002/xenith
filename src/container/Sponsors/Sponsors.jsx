ae6e533083b888a5cb290a03166c459df4b4d134
vimarshmishra37 <144466200+vimarshmishra37@users.noreply.github.com>
2024-01-19 13:00:48 +0530

    sponsors

--- src/container/Sponsors/Sponsors.jsx
+++ src/container/Sponsors/Sponsors.jsx
@@ -5,18 +5,18 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import './Sponsors.css';
-import aryan from '../../assets/Team Members/Raghav_Goel.png';
+
 export default function Sponsors() {
   return (
-    <div className='partner' >
+    <>
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={'auto'}
-        spaceBetween={'-1050px'}
+        slidesPerView={'1'}
+        spaceBetween={'-70%'}
         coverflowEffect={{
-          rotate: 50,
+          rotate: 40,
           stretch: 0,
           depth: 100,
           modifier: 1,
@@ -25,35 +25,37 @@ export default function Sponsors() {
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
+        allowSlideNext={true}
+        allowSlidePrev={true}
       >
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 1" />
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Sponsor 1" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 2" />
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="Sponsor 2" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 3" />
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="Sponsor 3" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan}alt="Sponsor 4" />
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="Sponsor 4" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 5" />
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="Sponsor 5" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 6" />
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="Sponsor 6" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 7" />
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="Sponsor 7" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 8" />
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="Sponsor 8" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={aryan} alt="Sponsor 9" />
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="Sponsor 9" />
         </SwiperSlide>
       </Swiper>
-    </div>
+    </>
   );
 }
