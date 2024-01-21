7de632c48599d6c59af023f5c0bf808f4c69414c
pankhuri92 <asthanap192@gmail.com>
2024-01-21 13:18:31 +0530

    3D-spring Gallery

---
+++ src/container/Gallery/Gallery-3D.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import Carousel from "react-spring-3d-carousel";
+// import uuidv4 from "uuid";
+import { config } from "react-spring";
+import p1 from '../../assets/Gallery/DSC_3238.jpg';
+import p2 from '../../assets/Gallery/DSC_3259.jpg';
+import p3 from '../../assets/Gallery/DSC_3396.jpg';
+import p4 from '../../assets/Gallery/DSC_3447.jpg';
+import p5 from '../../assets/Gallery/DSCN_20230204_134210_LMC.R12.jpg';
+import p6 from '../../assets/Gallery/DSCN_20230204_134910_LMC.R12.jpg';
+import p7 from '../../assets/Gallery/SB8_0446.jpg';
+import p8 from '../../assets/Gallery/SB8_0450.jpg';
+import './Gallery-3D.css'
+
+export const Gallery = () => {
+  const [state, setState] = useState({
+    goToSlide: 0,
+    offsetRadius: 2,
+    showNavigation: true,
+    config: config.gentle
+  });
+
+  let slides = [
+    {
+      content: <img src={p1} alt="1" />
+    },
+    {
+      content: <img src={p2} alt="2" />
+    },
+    {
+      content: <img src={p3} alt="3" />
+    },
+    {
+      content: <img src={p4} alt="4" />
+    },
+    {
+      content: <img src={p5} alt="5" />
+    },
+    {
+      content: <img src={p6} alt="6" />
+    },
+    {
+      content: <img src={p7} alt="7" />
+    },
+    {
+      content: <img src={p8} alt="8" />
+    }
+  ].map((slide, index) => {
+    return { ...slide, onClick: () => setState({ goToSlide: index }) };
+  });
+
+  const onChangeInput = (e) => {
+    setState({
+      [e.target.name]: parseInt(e.target.value, 10) || 0
+    });
+  };
+
+  let xDown = null;
+  let yDown = null;
+
+  const getTouches = (evt) => {
+    return (
+      evt.touches || evt.originalEvent.touches // browser API
+    );
+  };
+
+  const handleTouchStart = (evt) => {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  const handleTouchMove = (evt) => {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      /*most significant*/
+      if (xDiff > 0) {
+        /* left swipe */
+        setState({ goToSlide: state.goToSlide + 1 });
+      } else {
+        /* right swipe */
+        setState({ goToSlide: state.goToSlide - 1 });
+      }
+    } else {
+      if (yDiff > 0) {
+        /* up swipe */
+      } else {
+        /* down swipe */
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
+
+  return (
+    <div
+      style={{ width: "80%", height: "500px", margin: "5% auto" }}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+    >
+     <h1 className="vim">Gallery</h1>
+
+      <Carousel
+        slides={slides}
+        goToSlide={state.goToSlide}
+        offsetRadius={state.offsetRadius}
+        showNavigation={state.showNavigation}
+        animationConfig={state.config}
+      />
+      <div
+        style={{
+          margin: "0 auto",
+          marginTop: "2rem",
+          width: "50%",
+          display: "flex",
+          justifyContent: "space-around"
+        }}
+      ></div>
+    </div>
+  );
+};
