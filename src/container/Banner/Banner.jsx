a00d4b334a19bfebffe6132299108ebee2dd97d8
vimarshmishra37 <144466200+vimarshmishra37@users.noreply.github.com>
2024-01-26 22:10:24 +0530

    revert

--- src/container/Banner/Banner.jsx
+++ src/container/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import './Banner.css';
 
 const Banner = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const screenWidth = window.innerWidth;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,43 +17,37 @@ const Banner = () => {
     };
   }, []);
 
-  const scrollDownOpacity = 1 - Math.min(scrollPosition * 0.02, 1);
+  const calculateOpacity = () => {
+    const maxOpacity = 1;
+    const minOpacity = 0.2; // Adjust the minimum opacity as needed
+    const opacityRange = maxOpacity - minOpacity;
+
+    if (screenWidth <= 768) {
+      return maxOpacity - scrollPosition * 0.0015; // Keep full opacity on mobile
+    }
+
+    return maxOpacity - scrollPosition * 0.002;
+  };
+
 
   const parallaxStyle = (speed, rotateSpeed, scaleSpeed, horizontalSpeed) => {
-    const rotation = window.innerWidth > 950 ? `rotate(${scrollPosition * rotateSpeed}deg)` : '';
+    const rotation = screenWidth > 950 ? `rotate(${scrollPosition * rotateSpeed}deg)` : '';
 
     return {
       transform: `translateY(-${scrollPosition * speed}px) ${rotation} scale(${1 + scrollPosition * scaleSpeed}) translateX(${scrollPosition * horizontalSpeed}px)`,
-      opacity: 1 - Math.min(scrollPosition * 0.002, 1),
-      transition: 'transform 0.2s ease-out, opacity 0.2s ease-out',
+      opacity: calculateOpacity(),
     };
   };
 
-  const scrollDownStyle = {
-    transform: `translateY(-${scrollPosition * 4}px)`,
-    opacity: scrollDownOpacity,
-    transition: 'transform 0.2s ease-out, opacity 0.5s ease-out',
-  };
-
   return (
-    <div>
-      <div id="Home" className="landing-page-container">
-        <div className="landing-page" style={parallaxStyle(0.1, 0.1, 0.0005, 0)}>
-          <div className="parallax-background"></div>
-          <div className="layer1"></div>
-          <div className="layer2"></div>
-          <div className="content">
-            <h1>XENITH'24</h1>
-            <h5>The Incognito Realm</h5>
-          </div>
-        </div>
-      </div>
-      <div className="scroll-down-container" style={{ ...scrollDownStyle, bottom: '10px' }}>
-        <div className="wrapper">
-          <div className="box">
-            <div className="circle"></div>
-          </div>
-          <div className='scroll'>Scroll Down</div>
+    <div id="Home">
+      <div className="landing-page">
+        <div className="parallax-background" style={parallaxStyle(0.1, 0.1, 0.0005, 0)}></div>
+        <div className="layer1" style={parallaxStyle(0.0, 0, 0.0003, -0.2)}></div>
+        <div className="layer2" style={parallaxStyle(0.0, 0, 0.0002, 0.3)}></div>
+        <div className="content">
+          <h1>XENITH'24</h1>
+          <h5 >The Incognito Realm</h5>
         </div>
       </div>
     </div>
@@ -60,3 +55,7 @@ const Banner = () => {
 };
 
 export default Banner;
+
+
+
+
