459a68a7811ec13a4e3c385f9e535e7f9ffb3a3b
Yash Mittal <mittalyas1234@gmail.com>
2024-02-02 19:08:24 +0530

    changes done in chief guest section

--- src/components/ChiefGuest/ChiefGuest.jsx
+++ src/components/ChiefGuest/ChiefGuest.jsx
@@ -10,7 +10,14 @@ const ChiefGuest = () => {
                     <img src={photo} />
                 </div>
                 <p>Vikram Chopra is the Co-Founder and Chief Executive Officer of CARS24. He is a former Co-Founder of FabFurnish and Investment Analyst of Sequoia Capital and was Business Analyst at McKinsey & Company.
-                    Mr. Chopra received a BTech and MTech in Engineering at Indian Institute of Technology, Bombay, and earned an MBA in Finance at the Universityof Pennsylvania - The Wharton School.</p>
+                    Mr. Chopra received a BTech and MTech in Engineering at Indian Institute of Technology, Bombay, and earned an MBA in Finance at the Universityof Pennsylvania - The Wharton School.
+                    <ul>
+                        <li>CO-FOUNDER AND CEO @CARS24</li>
+                        <li>CO-FOUNDER @FABFURNISH</li>
+                        <li>EX-INVESTMENT ANALYST @SEQUOIA CAPITAL</li>
+                        <li>EX_BUSINESS ANALYST @MCKINSEY AND COMPANY</li>
+                    </ul>
+                </p>
             </div>
         </div>
     )
